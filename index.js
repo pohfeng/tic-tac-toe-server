d765c6d6d3f49d1cfaf7d780575437328144eb6a
pohfeng <pohfeng926@gmail.com>
2022-06-13 22:00:54 +0800

    limit cors

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: '*',
+    origin: 'tic-tac-toe-client-five.vercel.app',
   },
 });
 
