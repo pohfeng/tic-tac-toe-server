eaf23be0e0985a8a505768052f301d67b8bbd2ac
pohfeng <pohfeng926@gmail.com>
2022-06-13 15:57:39 +0800

    setup heroku

--- index.js
+++ index.js
@@ -6,9 +6,11 @@ const server = http.createServer(app);
 const { Server } = require('socket.io');
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: '*',
   },
 });
+
+const port = process.env.PORT || 3000;
 
 io.on('connection', (socket) => {
   console.log('a user connected: ', socket.id);
@@ -48,6 +50,6 @@ io.of('/').adapter.on('leave-room', (room, id) => {
   io.to(room).emit('leave-room', id);
 });
 
-server.listen(3001, () => {
-  console.log('listening on port 3001');
+server.listen(port, () => {
+  console.log(`listening on port ${port}`);
 });
